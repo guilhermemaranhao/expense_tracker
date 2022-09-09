effb28ce70c7a2cada9dde3217ea0c00510f89c3
guilherme <guilherme82@gmail.com>
2022-09-09 17:38:27 -0300

    Conclusão do capítulo 5

--- Gemfile
+++ Gemfile
@@ -6,4 +6,3 @@ gem 'rspec', '3.6.0'
 gem 'coderay', '1.1.1'
 gem 'rack-test', '0.7.0'
 gem 'sinatra', '2.1.0'
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,8 @@ RSpec.configure do |config|
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
   config.expect_with :rspec do |expectations|
+
+  config.filter_gems_from_backtrace 'rack', 'rack-test', 'sequel', 'sinatra'  
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
     # defined using `chain`, e.g.:
@@ -86,7 +88,7 @@ RSpec.configure do |config|
   # end of the spec run, to help surface which specs are running
   # particularly slow.
   config.profile_examples = 10
-
+=end
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
@@ -98,5 +100,4 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-=end
 end
